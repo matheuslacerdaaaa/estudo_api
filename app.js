07fca2767239959f91199df9d3b209e05442ee04
andradee0 <rafaandra157@gmail.com>
2024-08-02 09:16:30 -0300

    atualização (subtrair)

--- app.js
+++ app.js
@@ -15,10 +15,21 @@ resp.send("o resultado é" + ret)
 
 })
 
+servidor.get('/calculadora/subtrair/:n1/:n2', (req, resp) =>{
+
+    let n1 = Number(req.params.n1)
+    let n2 = Number(req.params.n2)
+
+    let total = n1 - n2
+
+    resp.send('o resultado é ' + total)
+}
+
+)
 
 
 
-servidor.listen(5050, 
+servidor.listen(5001, 
 
  () => console.log("oii"));   
 
