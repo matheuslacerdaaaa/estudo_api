e549bd7bcf6462856db20c1240ead9f8ce918dad
matheuslacerdaaaa <matheus301107@gmail.com>
2024-08-02 08:56:39 -0300

    oiii

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 import express from 'express';
 
 
-const servidor = express()
+const servidor = express();
 
-servidor.listen(5050, () =>
 
-() => console.log("oiii")
-);
+servidor.get('calculadora/somar/ :n1/:n2', (req,resp) => {
+
+let n1 = Number (req.params.n1)
+let n2 = Number (req.params.n2)
+
+let ret = n1 + n2
+
+resp.send("o resultado é" + ret)
+
+})
+
+
+
+
+servidor.listen(5050, 
+
+ () => console.log("oii"));   
+
 
